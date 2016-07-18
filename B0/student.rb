fcd3ffdf5e062eacef29c281118473d273d2f73a
dahuzi008 <604513270@qq.com>
2016-07-18 15:34:49 +0800

    add random

--- B0/student.rb
+++ B0/student.rb
@@ -16,9 +16,28 @@ class Student
 		puts "#{@id} #{@name} #{@gender} #{@age}"
 	end
 
+	#生成随机字串
+	def random_str()
+		#rand(36 ** rand(3..5)).to_s(36)
+		[*('a'..'z'),*('A'..'Z'),*(0..9)].shuffle[3..6].join
+	end
+
+	#循环生成器100个学生
+	def add_stu()
+		stus = Array.new(100){|i|}
+	end
+
+	def newpass(len)
+  	newpass = ""
+  	1.upto(len){ |i| newpass << rand(10).to_s}
+  	return newpass
+	end
+
 end
 
 #test
 stu1 = Student.new(1,"John","male",66)
 puts "Hello World!"
 stu1.info()
+puts stu1.random_str()
+puts stu1.newpass(3)
