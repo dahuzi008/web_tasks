796dd924ab9dbb214555016903bc5abafb7cf2cf
dahuzi008 <604513270@qq.com>
2016-07-18 14:42:54 +0800

    add attr_accessor

--- B0/student.rb
+++ B0/student.rb
@@ -1,9 +1,16 @@
 #!/usr/bin/ruby
 class Student
+	attr_accessor :id
+	attr_accessor :name
+	attr_accessor :gender
+	attr_accessor :age
+
 	def initialize(id,name,gender,age)
 		@id = id
 		@name = name
 		@gender = gender
 		@age = age
 	end
+	
+end
 	
