991fa01cd39198ea96f540f8732ee4071ff16b54
dahuzi008 <604513270@qq.com>
2016-07-20 18:58:24 +0800

    增加测试函数

--- B0/student.rb
+++ B0/student.rb
@@ -32,6 +32,7 @@ class Student
     "#{@id} #{@name} #{@gender} #{@age}"
   end
 
+end
 #生成随机字串
   # def random_str
   #   #rand(36 ** rand(3..5)).to_s(36)
@@ -39,7 +40,7 @@ class Student
   # end
 
 #生成随机字串
-  def self.newpass(len)
+  def newpass(len)
     chars = ("a".."z").to_a
     newpass = ""
     1.upto(len) { |i| newpass << chars[rand(chars.size-1)] }
@@ -47,7 +48,7 @@ class Student
   end
 
 #将students数据存到文件中
-  def self.store_stu(students)
+  def store_stu(students)
     if File::exists?("student.yml")
       stu_file = File.new("student.yml","w+")
     else
@@ -63,7 +64,6 @@ class Student
       stu_file.close
   end
 
-  i = 0
   if File::exists?("student.yml")
 	#如果文件存在，就直接读入
 	#arr = YAML::load(File.open('student.yml'))
@@ -71,6 +71,7 @@ class Student
     arr = IO.readlines("student.yml")
 	#p arr.length
     students = Array.new
+    i = 0
     while i < arr.length
       str = arr[i].split
       students[i] = Student.new(str[0].to_i, str[1], str[2].to_i, str[3].to_i)#to_i 转化成数字
@@ -96,17 +97,17 @@ class Student
   end
 
 #排序输出功能，按照id和age以及name首字母排序
-  def self.order_id(students)
+  def order_id(students)
     students = students.sort_by(:id)
     store_stu(students)
   end
 
-  def self.order_age(students)
+  def order_age(students)
     students = students.sort_by(:age)
     store_stu(students)
   end
 
-  def self.order_name(students)
+  def order_name(students)
     students = students.sort_by(:name)
     store_stu(students)
   end
@@ -116,7 +117,7 @@ class Student
 # order_age(students)
 
 #增删改查
-  def self.add_stu(students)
+  def add_stu(students)
     puts "请输入学生的name,gender('0'->男;'1'->女),age:(用空格隔开)"
     msg = gets
     str = msg.split
@@ -132,16 +133,15 @@ class Student
     @@max_id += 1
   end
 
-  def self.delete_stu(students)
+  def delete_stu(students)
     puts "请输入要删除的学生id："
     msg = gets
     delete_id = msg.to_i
     students[delete_id-1] = Student.new(delete_id,nil,nil,nil)
-    puts @@max_id
     store_stu(students)
   end
 
-  def self.edit_stu(students)
+  def edit_stu(students)
     puts "请输入要编辑的学生id："
     msg = gets
     edit_id = msg.to_i
@@ -152,7 +152,7 @@ class Student
     store_stu(students)
   end
 
-  def self.inqury_stu(students)
+  def inqury_stu(students)
     p students
   end
 #测试增删改查
@@ -162,44 +162,47 @@ class Student
 #inqury_stu(students)
 
 #整合一下
-  # def manage_stu(students)
-  #   puts "请输入操作码："
-  #   puts "1:增  2:删  3:改  4:查  5:排  0:退出"
-  #   opcode = gets
-  #   case opcode
-  #   when 0
-  #     puts "you're welcome!"
-  #     exit(0)
-  #   when 1
-  #     add_stu(students)
-  #   when 2
-  #     delete_stu(students)
-  #   when 3
-  #     edit_stu(students)
-  #   when 4
-  #     inqury_stu(students)
-  #   when 5
-  #     puts "请选择排序方式："
-  #     puts "1:byID  2:byAge  3:byName  "
-  #     iqcode = gets
-  #     case iqcode
-  #     when 1
-  #       order_id(students)
-  #     when 2
-  #       order_age(students)
-  #     when 3
-  #       order_name(students)
-  #     else
-  #       puts "输入有误，请重输！"
-  #     end
-  #   else
-  #     puts "输入有误，请重输！"
-  # end
+  def manage_stu(students)
+    while 1
+      puts "请输入操作码："
+      puts "1:增  2:删  3:改  4:查  5:排  0:退出"
 
-end
+      opcode = gets
+      case opcode.to_i
+      when 0
+        puts "you're welcome!"
+        exit(0)
+      when 1
+        add_stu(students)
+      when 2
+        delete_stu(students)
+      when 3
+        edit_stu(students)
+      when 4
+        inqury_stu(students)
+      when 5
+        puts "请选择排序方式："
+        puts "1:byID  2:byAge  3:byName  "
+        iqcode = gets
+        case iqcode.to_i
+        when 1
+          order_id(students)
+        when 2
+          order_age(students)
+        when 3
+          order_name(students)
+        else
+          puts "输入有误，请重输！"
+        end
+      else
+        puts "输入有误，请重输！"
+      end
+    end
+  end
 #test
 # stu1 = Student.new(1,"John","male",66)
 # puts "Hello World!"
 # stu1.info()
 # puts stu1.random_str()
 # puts stu1.newpass(3)
+manage_stu(students)
